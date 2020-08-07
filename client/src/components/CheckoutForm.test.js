da8b392b6751d08e6dfb65ba05bc6f7272f88329
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-07 11:53:03 -0400

    added test to insure App and checkoutform actually render, working on other tests now

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import App from '../App'
+
+test("App renders without crashing as well as Checkoutform", () => {
+    render(<App />)
+    render(<CheckoutForm />)
+})
 
 // Write up the two tests here and make sure they are testing what the title shows
 
